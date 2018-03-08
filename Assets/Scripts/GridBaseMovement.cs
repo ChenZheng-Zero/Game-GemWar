f1ccc9056ed5094e282b745b9624c2a6d2a84376
zhanghe <zhanghe@umich.edu>
2018-03-08 17:16:32 -0500

    improve grid base movement (add change direction feature)

--- Assets/Scripts/GridBaseMovement.cs
+++ Assets/Scripts/GridBaseMovement.cs
@@ -6,6 +6,7 @@ using InControl;
 public class GridBaseMovement : MonoBehaviour {
 
 	private bool moving = false;
+	private bool change_direction_moving = false;
 	private Vector3 direction;
 	private Vector3 previous_direction;
 	private Rigidbody rb;
@@ -17,6 +18,7 @@ public class GridBaseMovement : MonoBehaviour {
 
 	public float movement_speed = 4.0f;
 	public float input_magnitude_threshold = 0.8f;
+	public float change_direction_threshold = 0.2f;
 	public float holding_movement_speed = 2.0f;
 	public Vector3 initial_direction = Vector3.down;
 
@@ -75,15 +77,14 @@ public class GridBaseMovement : MonoBehaviour {
 			return;
 		}
 
-		animator.SetFloat ("horizontal", direction.x);
-		animator.SetFloat ("vertical", direction.y);
-
-		if (moving) {
-			return;
-		}
+		previous_direction = direction;
 
 		if (reborn.GetReborning() || rock_interaction.GetRemovingOpponentRock ()) {
 			animator.speed = 0.0f;
+			return;
+		}
+
+		if (change_direction_moving) {
 			return;
 		}
 
@@ -107,24 +108,71 @@ public class GridBaseMovement : MonoBehaviour {
 //			return;
 //		}
 
+		if (moving) {
+			if (previous_direction != input_direction && input_magnitude >= input_magnitude_threshold) {
+				float x_dec = transform.position.x - Mathf.Floor (transform.position.x);
+				float y_dec = transform.position.y - Mathf.Floor (transform.position.y);
+				float change_direction_offset = 0.0f;
 
-		previous_direction = direction;
-		if (input_magnitude != 0) {
-			direction = input_direction;
+				if (x_dec != 0.0f && y_dec != 0.0f) {
+					if (x_dec < 0.001f || x_dec > 0.999f) {
+						x_dec = 0.0f;
+					} else if (y_dec < 0.001 || y_dec > 0.999f) {
+						y_dec = 0.0f;
+					} else {
+						Debug.Log ("Player not on the grid!");
+						Debug.Log (x_dec);
+						Debug.Log (y_dec);
+					}
+				}
+
+				if (x_dec != 0.0f && x_dec < change_direction_threshold && previous_direction != Vector3.left) {
+					direction = Vector3.left;
+					change_direction_offset = x_dec;
+				} else if (x_dec > 1.0f - change_direction_threshold && previous_direction != Vector3.right) {
+					direction = Vector3.right;
+					change_direction_offset = 1.0f - x_dec;
+				} else if (y_dec != 0.0f && y_dec < change_direction_threshold && previous_direction != Vector3.down) {
+					direction = Vector3.down;
+					change_direction_offset = y_dec;
+				} else if (y_dec > 1.0f - change_direction_threshold && previous_direction != Vector3.up) {
+					direction = Vector3.up;
+					change_direction_offset = 1.0f - y_dec;
+				}
+
+				if (change_direction_offset != 0.0f) {
+					animator.SetFloat ("horizontal", direction.x);
+					animator.SetFloat ("vertical", direction.y);
+					StartCoroutine (ChangheDirectionCoroutine (change_direction_offset));
+				}
+			}
+		} else {
+			if (!moving && input_magnitude != 0) {
+				direction = input_direction;
+			}
+
+			animator.SetFloat ("horizontal", direction.x);
+			animator.SetFloat ("vertical", direction.y);
+
+			if (input_magnitude < input_magnitude_threshold) {
+				animator.speed = 0.0f;
+				return;
+			}
+
+			animator.speed = 1.0f;
+
+
+			if (CheckObstacle (transform.position + direction)) {
+				rb.velocity = Vector3.zero;
+				return;
+			}
+
+			StartCoroutine (MoveCoroutine ());
 		}
+	}
 
-		if (input_magnitude < input_magnitude_threshold) {
-			animator.speed = 0.0f;
-			return;
-		}
-
-		animator.speed = 1.0f;
-
-
-		if (CheckObstacle (transform.position + direction)) {
-			rb.velocity = Vector3.zero;
-			return;
-		}
+	private IEnumerator MoveCoroutine() {
+		moving = true;
 
 		if (gem_interaction.GetHolding ()) {
 			rb.velocity = holding_movement_speed * direction;
@@ -132,53 +180,58 @@ public class GridBaseMovement : MonoBehaviour {
 			rb.velocity = movement_speed * direction;
 		}
 
-		StartCoroutine (MoveCoroutine ());
-	}
-
-	private IEnumerator MoveCoroutine() {
-		moving = true;
-		float origin_speed = rb.velocity.magnitude;
-		Vector3 origin_position = transform.position;
+		Vector3 origin_velocity = rb.velocity;
+		Vector3 origin_position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0.0f);
 		Vector3 moving_direction = direction;
 
-//		bool cancel = false;
-//		if (moving_direction != previous_direction) {
-//			for (float t = 0.0f; t < 0.1f; t += Time.deltaTime) {
-//				if (GetInput () != moving_direction) {
-//					cancel = true;
-//					break;
-//				}
-//				yield return null;
-//			}
-//		}
-//
-//		if (cancel) {
-//			Debug.Log ("cancel");
-//			transform.position = origin_position;
-//		} else {
-			bool blocked = false;
-			float moving_time = 1.0f / origin_speed;
-//			if (moving_direction != previous_direction) {
-//				moving_time -= 0.1f;
-//			}
+		bool blocked = false;
+		bool stop = false;
+		float moving_time = 1.0f / origin_velocity.magnitude;
 
-			for (float t = 0.0f; t < moving_time; t += Time.deltaTime) {
-				yield return null;
-				if (CheckObstacle (origin_position + direction)) {
-					blocked = true;
-					break;
-				}
+		for (float t = 0.0f; t < moving_time; t += Time.deltaTime) {
+			rb.velocity = origin_velocity;
+			yield return null;
+			if (CheckObstacle (origin_position + moving_direction)) {
+				blocked = true;
+				break;
+			} else if (change_direction_moving) {
+				stop = true;
+				break;
 			}
-			
-			if (blocked) {
-				transform.position = origin_position;
-			} else {
-				transform.position = origin_position + direction;
-			}
-//		}
+		}
+		
+		rb.velocity = Vector3.zero;
+
+		if (blocked) {
+			transform.position = origin_position;
+		} else if (!stop) {
+			transform.position = origin_position + moving_direction;
+		}
+
+		moving = false;
+	}
+
+	private IEnumerator ChangheDirectionCoroutine(float offset) {
+		change_direction_moving = true;
+
+		if (gem_interaction.GetHolding ()) {
+			rb.velocity = holding_movement_speed * direction;
+		} else {
+			rb.velocity = movement_speed * direction;
+		}
+
+		Vector3 origin_velocity = rb.velocity;
+		Vector3 target_position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), 0.0f);
+
+
+		for (float t = 0.0f; t < offset / origin_velocity.magnitude; t += Time.deltaTime) {
+			rb.velocity = origin_velocity;
+			yield return null;
+		}
 
 		rb.velocity = Vector3.zero;
-		moving = false;
+		transform.position = target_position;
+		change_direction_moving = false;
 	}
 
 	public Vector3 GetDirection() {
