004b5e5d4262f712d83f60350917eb5ffe8155fd
zhanghe <zhanghe@umich.edu>
2018-03-24 22:43:13 -0400

    able to drop gem

--- Assets/Scripts/GemInteraction.cs
+++ Assets/Scripts/GemInteraction.cs
@@ -6,6 +6,7 @@ using InControl;
 public class GemInteraction : MonoBehaviour {
 
 	private bool holding = false;
+	private bool key_down = false;
 	private GameObject gem = null;
 	private InputDevice input_device;
 	private Reborn reborn;
@@ -24,12 +25,56 @@ public class GemInteraction : MonoBehaviour {
 			return;
 		}
 
-		if (input_device.Action1 && !holding) {
-			Collider collider = PublicFunctions.instance.FindObjectOnPosition (transform.position + grid_base_movement.GetDirection ());
-			if (collider && (collider.CompareTag ("gem_blue") || collider.CompareTag ("gem_red"))) {
-				collider.GetComponent<BoxCollider> ().enabled = false;
-				collider.transform.position = transform.position + Vector3.up * 0.8f;
-				Hold (collider.gameObject);
+		if (input_device.Action1 && !key_down) {
+			key_down = true;
+
+			if (holding) {
+				Vector3 facing_position = transform.position + grid_base_movement.GetDirection () + GetOffset ();
+				Collider collider = PublicFunctions.instance.FindObjectOnPosition (facing_position);
+				if (!collider || !(collider.CompareTag ("base_blue") || collider.CompareTag ("base_red") || collider.CompareTag ("gem_blue") || collider.CompareTag ("gem_red") ||
+					collider.CompareTag ("wall") || collider.CompareTag ("rock_blue") || collider.CompareTag ("rock_red") || collider.CompareTag ("reborn_blue") || collider.CompareTag ("reborn_red") ||
+					collider.CompareTag ("player1") || collider.CompareTag ("player2") || collider.CompareTag ("player3") || collider.CompareTag ("player4"))) {
+
+					holding = false;
+					gem.transform.parent = null;
+					gem.transform.position = facing_position;
+					gem.GetComponent<BoxCollider> ().enabled = true;
+				}
+			} else {
+				Collider collider = PublicFunctions.instance.FindObjectOnPosition (transform.position + grid_base_movement.GetDirection ());
+				if (collider && (collider.CompareTag ("gem_blue") || collider.CompareTag ("gem_red"))) {
+					collider.GetComponent<BoxCollider> ().enabled = false;
+					collider.transform.position = transform.position + Vector3.up * 0.8f;
+					Hold (collider.gameObject);
+				}
+			}
+		}
+
+		if (!input_device.Action1 && key_down) {
+			key_down = false;
+		}
+	}
+
+	private Vector3 GetOffset() {
+		Vector3 direction = grid_base_movement.GetDirection ();
+
+		if (direction == Vector3.up || direction == Vector3.down) {
+			float dec = transform.position.y - Mathf.Floor (transform.position.y);
+			if (dec > 0.0f && dec < 0.5f) {
+				return new Vector3 (0.0f, 0.0f - dec, 0.0f);
+			} else if (dec > 0.5f && dec < 1.0f) {
+				return new Vector3 (0.0f, 1.0f - dec, 0.0f);
+			} else {
+				return Vector3.zero;
+			}
+		} else {
+			float dec = transform.position.x - Mathf.Floor (transform.position.x);
+			if (dec > 0.0f && dec < 0.5f) {
+				return new Vector3 (0.0f - dec, 0.0f, 0.0f);
+			} else if (dec > 0.5f && dec < 1.0f) {
+				return new Vector3 (1.0f - dec, 0.0f, 0.0f);
+			} else {
+				return Vector3.zero;
 			}
 		}
 	}
