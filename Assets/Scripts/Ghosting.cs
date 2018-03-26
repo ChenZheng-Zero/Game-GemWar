54558559fecb1403a0a25e4372c8a2d50ba4eb1a
zhanghe <zhanghe@umich.edu>
2018-03-25 22:31:33 -0400

    fix ghosting during reborn

--- Assets/Scripts/Ghosting.cs
+++ Assets/Scripts/Ghosting.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Ghosting : MonoBehaviour {
 
 	private bool actived = false;
+	private Reborn reborn;
 	private ParticleSystem ps;
 	private BuffController buff_controller;
 
 	void Start () {
+		reborn = GetComponent<Reborn> ();
 		ps = GetComponent<ParticleSystem> ();
 		buff_controller = GetComponent<BuffController> ();
 
@@ -19,7 +21,7 @@ public class Ghosting : MonoBehaviour {
 		if (buff_controller.GetSpeedCoefficient () != 1.0f && !actived) {
 			actived = true;
 			ps.Play ();
-		} else if (buff_controller.GetSpeedCoefficient () == 1.0f && actived) {
+		} else if ((reborn.GetReborning() || buff_controller.GetSpeedCoefficient () == 1.0f) && actived) {
 			actived = false;
 			ps.Stop ();
 		}
